8428b0994a7aa589bde2f9c7798303f9d78afa63
Shameer C <shameer806@gmail.com>
2011-05-10 10:58:36 +0530

    First commit, almost functioning

---
+++ Database.php
@@ -0,0 +1,143 @@
+<?php
+
+    /**
+     * High level database abstraction class.
+     * This class is intented to give an abstraction to basic Driver dependent
+     * classes. Instead of directly calling them we can specify the type and
+     * database connection details here in a standard dsn (Database Source Name)
+     * format (given below). This will help us incase if we need a dbal to be
+     * integrated to the system
+     * 
+     * @package Database
+     * @category Database
+     * @author Shameer
+     */
+
+    //Default dsn values as given in the config file
+    $GLOBALS['_default_dsn'] =  array('dbhost'  => DB_HOST,
+                                      'phptype' => DB_TYPE,
+                                      'password'=> DB_PASS,
+                                      'port'    => '3306',
+                                      'username'=> DB_USER,
+                                      'database'=> DB
+                                     );
+    //Start class Database
+    class Database {
+        /**
+         * @var string Used to set the error informations
+         */
+        public  $errorInfo;
+
+        /**
+         * @var array Used to store the default dsn of current instance
+         */
+        private $default_dsn;
+
+        /**
+         * @var array Stores instances
+         */
+        public static $_instances;
+        /**
+         * Constructor
+         * @param array $dsn
+         */
+        public function  __construct($dsn = null) {
+            if($dsn) {
+                if($this->isValid($dsn))
+                    $this->default_dsn = $dsn;
+                else {
+                    throw new Exception("Invalide data source name");
+                }
+            }
+            else {
+                $this->default_dsn = $GLOBALS['_default_dsn'];
+            }
+        }
+        /**
+         * @param  mixed   $dsn Not required. Returns an instance of database object
+         *                 that corresponds to the dsn. Only one object will be
+         *                 created for a particular dsn.
+         * @return mixed   Database object of the type @var $dsn['phptype'].
+         */
+        public function factory($dsn = null)
+        {
+            $dsn = $dsn == null ? Database::getDsn() : $dsn;
+            //Check if the dsn is valid
+            if (Database::isValid($dsn)) {
+                $phpType  = $dsn['phptype'];
+                $database = $dsn['database'];
+            } else {
+                throw new Exception("Invalid dsn. Please check the specifications");
+            }
+            //If an object for $phpType and $database already created , it will return
+            //that, else will create a new instance.
+            if (!($db = self::$_instances[$phpType][$database])) {
+                try { 
+                     $db = Database::loadDriver($phpType);
+                     $db->setDsn($dsn);
+                     $db->dbConnect();
+                }
+                catch (Exception $e){
+                    echo $e->getMessage();
+                }
+                // Add the object to created instances array
+                self::$_instances[$phpType][$database] = $db;
+            }
+            return $db;
+        }
+
+        /**
+         * @param  mixed $type  Database driver type
+         * @return mixed Object of database driver
+         */
+        public static function loadDriver($type)
+        { 
+            $file = _SYSTEM_ . 'database' .DS. 'driver' .DS. $type .'.php';
+            require_once $file;
+            $className ='Driver_'.$type;
+            return new $className();
+        }
+
+        /**
+         * Returns an object of global dsn
+         */
+        public static function singleton()
+        {
+            return Database::factory($GLOBALS['_default_dsn']);
+        }
+        
+        /**
+         * @return array dsn for current instace
+         */
+        public function getDsn()
+        {
+            if(isset($this) && isset($this->default_dsn)) {
+                return $this->default_dsn;
+            }
+            else {
+                return $GLOBALS['_default_dsn'];
+            }
+        }
+        
+        /**
+         * @param  array $dsn "Data source name"
+         * @return boolean true if dsn is valid
+         *                 else false
+         */
+        public function isValid($dsn)
+        {
+            if (empty($dsn['phptype'])
+                    || empty($dsn['dbhost'])
+                    || empty($dsn['username'])
+                    || empty($dsn['database']))
+                return false;
+            else {
+                $dsn =array_merge($GLOBALS['_default_dsn'], $dsn);
+                self::$_instances['_dsn'][] = $dsn;
+                return true;
+            }
+        }
+
+    }
+
+?>
---
+++ driver/Mysqli.php
@@ -0,0 +1,346 @@
+<?php 
+
+class Driver_Mysqli
+{
+	private 		$mysqli; 
+	private 		$HostName;
+	private			$UserName;
+	private			$Password;
+	private			$DatabaseName;
+	public			$ErrorInfo;
+        private                 $result;
+        private                 $cachedQueries = array();
+		 		
+	function __construct()
+	{
+            
+	}
+        function  __destruct() {
+                mysqli_free_result($this->result);
+                $this->stmt->close();
+        }
+	function setDsn($dns)
+        {
+                $this->HostName			=	$dns['dbhost'];
+		$this->UserName			=	$dns['username'];
+		$this->Password			=	$dns['password'];
+		$this->DatabaseName		=	$dns['database'];
+        }
+		
+	#	The following method establish a connection with the database server and on success return TRUE, on failure return FALSE
+	#	On failure ErrorInfo property contains the error information.
+	function dbConnect()
+	{
+		$this->mysqli 	= 	new mysqli($this->HostName, $this->UserName, $this->Password, $this->DatabaseName);
+		if(!$this->mysqli) { 
+			$this->ErrorInfo	=	mysqli_connect_error();
+			return FALSE;
+		} else {
+			return TRUE;
+		}
+	} # Close method dbConnect
+	
+	function dbClose()	{		
+		$this->mysqli->close();				
+	} # Close method dbClose
+	
+	# On insert, update it returns TRUE,  and on select it returns result set object
+	function setQuery($Query)
+	{
+		$ExecStatus		=	$this->mysqli->query($Query);
+		if($ExecStatus	===	FALSE) {
+			$this->ErrorInfo	=	$this->mysqli->error;
+			return FALSE;
+		} else {
+			return $ExecStatus;
+		} 
+	} # Close method setQuery			
+		
+		
+		
+	
+	# On Success returns number of records corresponding to the query, else return 0	
+	function numberOfRecords($Query)
+	{
+		$RowCount	=	0;
+                $this->result	=	$this->mysqli->query($Query);
+		if($this->result) {
+			$RowCount	=	 $this->result->num_rows;
+			return $RowCount;
+		} else {
+			$this->ErrorInfo	=	$this->mysqli->error;
+			return $RowCount;
+		}
+	} # Close numberOfRecords method
+	
+	
+	# Returns an array of rows in the result set
+	function readValues($Query)
+	{
+		$ResultData		=	array();
+		$this->result		=	$this->mysqli->query($Query);
+		
+		if($this->result) {
+			$RowCount		=	$this->result->num_rows;
+			for($i=0; $i<$RowCount; $i++)
+				$ResultData[$i]	=	$this->result->fetch_array();
+			return $ResultData;
+		} else {
+			$this->ErrorInfo	=	$this->mysqli->error;
+			return $ResultData;
+		}	
+	} # Close method readValues
+	
+	# Return a single row 
+	function readValue($Query)
+	{
+		$ResultData		=	array();
+		$this->result		=	$this->mysqli->query($Query);
+		
+		if($this->result) {
+			$ResultData[0]	=	$this->result->fetch_array();
+			return $ResultData[0];
+		} else {
+			$this->ErrorInfo	=	$this->mysqli->error;
+			return $ResultData;
+		}		
+	} # Close method readValue
+
+	# Method returns the last insert Id of this connection	
+	function getInsertId()
+	{
+		return $this->mysqli->insert_id;
+	}
+	
+	
+	function readField($Query)
+	{
+		$ResultData		=	array();
+		$this->result		=	$this->mysqli->query($Query);
+		
+		if($this->result) {
+			$ResultData[0]	=	$this->result->fetch_array();
+			return $ResultData[0];
+		} else {
+			$this->ErrorInfo	=	$this->mysqli->error;
+			return $ResultData;
+		}		
+	} # Close method readValue
+
+	# Method to execute Stored Procedures with return value
+	function execProc($qry)
+	{
+		$result	= array();
+		$result = $this->mysqli->query($qry);
+		if($result) {
+			$row = $result->fetch_array();
+			return $row;
+			$result->free();
+		} else { 
+			$this->ErrorInfo	=	$this->mysqli->error;
+			return $result;
+		}
+	}
+	
+	function execProcOne($qry)
+	{
+		$result	= array();
+		$res	= array();
+		$result = $this->mysqli->query($qry);
+		if($result) {
+			while ($row = $result->fetch_array())
+			{
+				$res[]	= $row;
+			}
+			return $res;
+			$result->free();
+		} else { 
+			$this->ErrorInfo	=	$this->mysqli->error;
+			return $result;
+		}
+	}
+	
+	function setProc($Query)
+	{
+		$ExecStatus		=	$this->mysqli->query($Query);
+		if($ExecStatus	===	FALSE) {
+			$this->ErrorInfo	=	$this->mysqli->error;
+			return FALSE;
+		} else {
+			return $ExecStatus;
+		} 
+	}
+        function escape($text)
+        {
+            $text = mysqli_real_escape_string($this->mysqli,$text);
+            return $text;
+        }
+        function quoteEscape($text) {
+            $type = gettype($text);
+            switch ($type) {
+                case 'integer':
+                    return $text;
+                    break;
+                default:
+                      return "'".$this->escape($text)."'";
+            }
+        }
+        
+        /**
+         * @param mixed $query   Query to be prepared for executing
+         *                      multiple times
+         * Usage  "SELECT * FROM table_name WHERE column = ? "
+         *        "INSERT INTO table_name(field1,field2) VALUES(?,?) ";
+         * returns true if the query is a valid mysql statement else throws an
+         * exception
+         * 
+         */
+        function prepare($query)
+        {
+            if(!($this->stmt = $this->isCached($query))) {
+                $this->stmt = $this->mysqli->prepare($query);
+                $this->cacheQuery($query, $this->stmt);
+            }
+            if(!($this->stmt instanceof  mysqli_stmt))
+                throw new Exception ('Invalide query : '.$this->mysqli->error);
+            else {
+                return true;
+            }
+        }
+
+        /**
+         *
+         * @param mixed   $values  Array or single value that need to be processed
+         *                         using already created prepared statment.
+         * @param boolean $isManip Whether the query is a data manipulation
+         *                         query or not. 
+         * @return mixed  $output  If @see $isManip is true return value will be
+         *                         boolean else it will be an array
+         * Usage execute(value); or execute(array(value1,value2), true);
+         */
+        function execute($values, $isManip = false, &$stmt = '' )
+        {
+            $stmt = ($stmt == '') ? $this->stmt : $stmt;
+            //check whether the statment is a valid prepared statment.
+            if(!($stmt  instanceof  mysqli_stmt))
+                throw new Exception ('Invalide query : '.$this->mysqli->error );
+            $ptype ='';
+
+            //Check if  $values is an array or not
+            if(is_array($values)) {
+                // Check the datatype of each value in the array and generate
+                // corresponding typestring $ptype
+                foreach($values as $value) {
+                    if(is_int($value))
+                        $ptype .= 'i';
+                    elseif(is_float($value))
+                        $ptype .='d';
+                    else
+                        $ptype .= 's';
+                    $params[] = $value;
+                }
+            }
+            else {
+                if(is_int($values))
+                    $ptype .= 'i';
+                elseif(is_float($values))
+                    $ptype .='d';
+                else
+                    $ptype .= 's';
+                $params[] = $values;
+            }
+            
+            //Combine the $params array, $this->stmt and $ptype to be passed as
+            //an argument for call_user_func_array()
+            array_unshift($params,$stmt ,$ptype);
+            call_user_func_array('mysqli_stmt_bind_param', $this->refValues($params));
+
+            //Execute the prepared statment
+            $output = $stmt->execute();
+
+            // Build the output array if its not a data manipulation query
+            if($isManip === false) {
+                $data   = $stmt->result_metadata();
+                $fields = array();
+                $out = array();
+
+                $fields[0] = $stmt;
+                $count = 1;
+
+                while($field = mysqli_fetch_field($data)) {
+                    $fields[$count] = &$out[$field->name];
+                    $count++;
+                }
+                call_user_func_array('mysqli_stmt_bind_result', $fields);
+                $i=0;
+                while($stmt->fetch()) {
+                    $result[] = $out;
+                }
+               if(count($result) == 1 )
+                  $output =  array_pop($result);
+               else
+                   $output = $result;
+            }
+            if(isset($output))
+                return $output;
+            else
+                return false;
+        }
+
+        // Php version above 5.3 need arguments of mysqli_stmt_bind_param
+        // function to be passed by reference. This function will recieve an array
+        // and will change each array elements to a reference to array values.
+        function refValues($params)
+        {
+            if (strnatcmp(phpversion(),'5.3') >= 0)
+            {
+                for($i=1; $i<count($params); $i++)
+                    $params[$i] = &$params[$i];
+            }
+            return $params;
+        }
+        function numColumns()
+        {
+            $cols = @mysqli_num_fields($this->result);
+        }
+
+        function getColumns()
+        {
+            $column = 0;
+            $columns = array();
+            $numCols = $this->numColumns();
+            for($column = 0; $column < $numCols; $column++) {
+                   $columnInfo = @mysqli_fetch_field_direct($this->result, $column);
+                   $columns[$columnInfo->name] = $column;
+            }
+            print_r($columns);
+        }
+
+        /**
+         *  Add a prepared statement to an array so tahat it can be reused
+         *  if the same query is executed again
+         * @param mixed $name
+         * @param mysqli_stmt $stmt
+         */
+        function cacheQuery($name,$stmt)
+        {
+            $this->cachedQueries[$name] = $stmt;
+        }
+        /**
+         * Check if query is cached or not. If yes returns the statement object
+         * @param mixed $name
+         * @return mysqli_stmt
+         */
+        function isCached($name)
+        {
+            if(array_key_exists($name, $this->cachedQueries)) {
+                return $this->cachedQueries[$name];
+            }
+            else {
+                return false;
+            }
+        }
+
+	
+} # Close class definition
+?>
