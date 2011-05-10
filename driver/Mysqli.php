5ed4d1cc6b63f1b05e5cbd4438faa4b40cf2b486
Shameer C <shameer806@gmail.com>
2011-05-10 17:40:21 +0530

    Added bit of comments

--- driver/Mysqli.php
+++ driver/Mysqli.php
@@ -1,180 +1,211 @@
-<?php 
+<?php
+    
+    /**
+     * Mysqli db driver
+     * Part of database class
+     */
 
-class Driver_Mysqli
-{
-	private 		$mysqli; 
-	private 		$HostName;
-	private			$UserName;
-	private			$Password;
-	private			$DatabaseName;
-	public			$ErrorInfo;
-        private                 $result;
-        private                 $cachedQueries = array();
-		 		
-	function __construct()
-	{
+    class Driver_Mysqli {
+
+        private $mysqli;
+        private $HostName;
+        private $UserName;
+        private $Password;
+        private $DatabaseName;
+        public  $ErrorInfo;
+        private $result;
+        private $cachedQueries = array();
+
+        function __construct() {
             
-	}
-        function  __destruct() {
-                mysqli_free_result($this->result);
-                $this->stmt->close();
         }
-	function setDsn($dns)
-        {
-                $this->HostName			=	$dns['dbhost'];
-		$this->UserName			=	$dns['username'];
-		$this->Password			=	$dns['password'];
-		$this->DatabaseName		=	$dns['database'];
+
+        function __destruct() {
+            mysqli_free_result($this->result);
+            $this->stmt->close();
         }
-		
-	#	The following method establish a connection with the database server and on success return TRUE, on failure return FALSE
-	#	On failure ErrorInfo property contains the error information.
-	function dbConnect()
-	{
-		$this->mysqli 	= 	new mysqli($this->HostName, $this->UserName, $this->Password, $this->DatabaseName);
-		if(!$this->mysqli) { 
-			$this->ErrorInfo	=	mysqli_connect_error();
-			return FALSE;
-		} else {
-			return TRUE;
-		}
-	} # Close method dbConnect
-	
-	function dbClose()	{		
-		$this->mysqli->close();				
-	} # Close method dbClose
-	
-	# On insert, update it returns TRUE,  and on select it returns result set object
-	function setQuery($Query)
-	{
-		$ExecStatus		=	$this->mysqli->query($Query);
-		if($ExecStatus	===	FALSE) {
-			$this->ErrorInfo	=	$this->mysqli->error;
-			return FALSE;
-		} else {
-			return $ExecStatus;
-		} 
-	} # Close method setQuery			
-		
-		
-		
-	
-	# On Success returns number of records corresponding to the query, else return 0	
-	function numberOfRecords($Query)
-	{
-		$RowCount	=	0;
-                $this->result	=	$this->mysqli->query($Query);
-		if($this->result) {
-			$RowCount	=	 $this->result->num_rows;
-			return $RowCount;
-		} else {
-			$this->ErrorInfo	=	$this->mysqli->error;
-			return $RowCount;
-		}
-	} # Close numberOfRecords method
-	
-	
-	# Returns an array of rows in the result set
-	function readValues($Query)
-	{
-		$ResultData		=	array();
-		$this->result		=	$this->mysqli->query($Query);
-		
-		if($this->result) {
-			$RowCount		=	$this->result->num_rows;
-			for($i=0; $i<$RowCount; $i++)
-				$ResultData[$i]	=	$this->result->fetch_array();
-			return $ResultData;
-		} else {
-			$this->ErrorInfo	=	$this->mysqli->error;
-			return $ResultData;
-		}	
-	} # Close method readValues
-	
-	# Return a single row 
-	function readValue($Query)
-	{
-		$ResultData		=	array();
-		$this->result		=	$this->mysqli->query($Query);
-		
-		if($this->result) {
-			$ResultData[0]	=	$this->result->fetch_array();
-			return $ResultData[0];
-		} else {
-			$this->ErrorInfo	=	$this->mysqli->error;
-			return $ResultData;
-		}		
-	} # Close method readValue
+        
+        /**
+         *
+         * @param mixed $dns Array of connection information
+         */
+        function setDsn($dns) {
+            $this->HostName = $dns['dbhost'];
+            $this->UserName = $dns['username'];
+            $this->Password = $dns['password'];
+            $this->DatabaseName = $dns['database'];
+        }
 
-	# Method returns the last insert Id of this connection	
-	function getInsertId()
-	{
-		return $this->mysqli->insert_id;
-	}
-	
-	
-	function readField($Query)
-	{
-		$ResultData		=	array();
-		$this->result		=	$this->mysqli->query($Query);
-		
-		if($this->result) {
-			$ResultData[0]	=	$this->result->fetch_array();
-			return $ResultData[0];
-		} else {
-			$this->ErrorInfo	=	$this->mysqli->error;
-			return $ResultData;
-		}		
-	} # Close method readValue
+        #	The following method establish a connection with the database server and on success return TRUE, on failure return FALSE
+        #	On failure ErrorInfo property contains the error information.
 
-	# Method to execute Stored Procedures with return value
-	function execProc($qry)
-	{
-		$result	= array();
-		$result = $this->mysqli->query($qry);
-		if($result) {
-			$row = $result->fetch_array();
-			return $row;
-			$result->free();
-		} else { 
-			$this->ErrorInfo	=	$this->mysqli->error;
-			return $result;
-		}
-	}
-	
-	function execProcOne($qry)
-	{
-		$result	= array();
-		$res	= array();
-		$result = $this->mysqli->query($qry);
-		if($result) {
-			while ($row = $result->fetch_array())
-			{
-				$res[]	= $row;
-			}
-			return $res;
-			$result->free();
-		} else { 
-			$this->ErrorInfo	=	$this->mysqli->error;
-			return $result;
-		}
-	}
-	
-	function setProc($Query)
-	{
-		$ExecStatus		=	$this->mysqli->query($Query);
-		if($ExecStatus	===	FALSE) {
-			$this->ErrorInfo	=	$this->mysqli->error;
-			return FALSE;
-		} else {
-			return $ExecStatus;
-		} 
-	}
-        function escape($text)
-        {
-            $text = mysqli_real_escape_string($this->mysqli,$text);
+        function dbConnect() {
+            $this->mysqli = new mysqli($this->HostName, $this->UserName, $this->Password, $this->DatabaseName);
+            if (!$this->mysqli) {
+                $this->ErrorInfo = mysqli_connect_error();
+                return FALSE;
+            } else {
+                return TRUE;
+            }
+        }
+
+
+
+        function dbClose() {
+            $this->mysqli->close();
+        }
+
+
+        /**
+         *  Function to perform dml operations
+         * @param mixed $Query Query to be executed
+         * @return query execution status 
+         */
+
+        function setQuery($Query) {
+            $ExecStatus = $this->mysqli->query($Query);
+            if ($ExecStatus === FALSE) {
+                $this->ErrorInfo = $this->mysqli->error;
+                return FALSE;
+            } else {
+                return $ExecStatus;
+            }
+        }
+
+        
+        # On Success returns number of records corresponding to the query, else return 0	
+
+        function numberOfRecords($Query) {
+            $RowCount = 0;
+            $this->result = $this->mysqli->query($Query);
+            if ($this->result) {
+                $RowCount = $this->result->num_rows;
+                return $RowCount;
+            } else {
+                $this->ErrorInfo = $this->mysqli->error;
+                return $RowCount;
+            }
+        }
+
+
+        /**
+         *  Function to return set of rows 
+         * @param mixed $Query Query to be executed
+         * @return two dimentional array of records 
+         */
+
+        function readValues($Query) {
+            $ResultData = array();
+            $this->result = $this->mysqli->query($Query);
+
+            if ($this->result) {
+                $RowCount = $this->result->num_rows;
+                for ($i = 0; $i < $RowCount; $i++)
+                    $ResultData[$i] = $this->result->fetch_array();
+                return $ResultData;
+            } else {
+                $this->ErrorInfo = $this->mysqli->error;
+                return $ResultData;
+            }
+        }
+
+
+        /**
+         *  Function to return a single row
+         * @param type Query to be executed
+         * @return single array of record
+         */
+
+        function readValue($Query) {
+            $ResultData = array();
+            $this->result = $this->mysqli->query($Query);
+
+            if ($this->result) {
+                $ResultData[0] = $this->result->fetch_array();
+                return $ResultData[0];
+            } else {
+                $this->ErrorInfo = $this->mysqli->error;
+                return $ResultData;
+            }
+        }
+
+
+        /**
+         *  Method to return last inserted id
+         * @return int last inserted id
+         */
+
+        function getInsertId() {
+            return $this->mysqli->insert_id;
+        }
+        
+        # this function is not necessary
+        function readField($Query) {
+            $ResultData = array();
+            $this->result = $this->mysqli->query($Query);
+
+            if ($this->result) {
+                $ResultData[0] = $this->result->fetch_array();
+                return $ResultData[0];
+            } else {
+                $this->ErrorInfo = $this->mysqli->error;
+                return $ResultData;
+            }
+        }
+
+
+        # Method to execute Stored Procedures with return value
+
+        function execProc($qry) {
+            $result = array();
+            $result = $this->mysqli->query($qry);
+            if ($result) {
+                $row = $result->fetch_array();
+                return $row;
+                $result->free();
+            } else {
+                $this->ErrorInfo = $this->mysqli->error;
+                return $result;
+            }
+        }
+
+        function execProcOne($qry) {
+            $result = array();
+            $res = array();
+            $result = $this->mysqli->query($qry);
+            if ($result) {
+                while ($row = $result->fetch_array()) {
+                    $res[] = $row;
+                }
+                return $res;
+                $result->free();
+            } else {
+                $this->ErrorInfo = $this->mysqli->error;
+                return $result;
+            }
+        }
+
+        function setProc($Query) {
+            $ExecStatus = $this->mysqli->query($Query);
+            if ($ExecStatus === FALSE) {
+                $this->ErrorInfo = $this->mysqli->error;
+                return FALSE;
+            } else {
+                return $ExecStatus;
+            }
+        }
+        
+        /**
+         * Method to escape special characters from a string
+         * @param string $text
+         * @return escaped string 
+         */
+        
+        function escape($text) {
+            $text = mysqli_real_escape_string($this->mysqli, $text);
             return $text;
         }
+
         function quoteEscape($text) {
             $type = gettype($text);
             switch ($type) {
@@ -182,10 +213,10 @@ class Driver_Mysqli
                     return $text;
                     break;
                 default:
-                      return "'".$this->escape($text)."'";
+                    return "'" . $this->escape($text) . "'";
             }
         }
-        
+
         /**
          * @param mixed $query   Query to be prepared for executing
          *                      multiple times
@@ -195,14 +226,13 @@ class Driver_Mysqli
          * exception
          * 
          */
-        function prepare($query)
-        {
-            if(!($this->stmt = $this->isCached($query))) {
+        function prepare($query) {
+            if (!($this->stmt = $this->isCached($query))) {
                 $this->stmt = $this->mysqli->prepare($query);
                 $this->cacheQuery($query, $this->stmt);
             }
-            if(!($this->stmt instanceof  mysqli_stmt))
-                throw new Exception ('Invalide query : '.$this->mysqli->error);
+            if (!($this->stmt instanceof mysqli_stmt))
+                throw new Exception('Invalide query : ' . $this->mysqli->error);
             else {
                 return true;
             }
@@ -218,22 +248,21 @@ class Driver_Mysqli
          *                         boolean else it will be an array
          * Usage execute(value); or execute(array(value1,value2), true);
          */
-        function execute($values, $isManip = false, &$stmt = '' )
-        {
+        function execute($values, $isManip = false, &$stmt = '') {
             $stmt = ($stmt == '') ? $this->stmt : $stmt;
             //check whether the statment is a valid prepared statment.
-            if(!($stmt  instanceof  mysqli_stmt))
-                throw new Exception ('Invalide query : '.$this->mysqli->error );
-            $ptype ='';
+            if (!($stmt instanceof mysqli_stmt))
+                throw new Exception('Invalide query : ' . $this->mysqli->error);
+            $ptype = '';
 
             //Check if  $values is an array or not
-            if(is_array($values)) {
+            if (is_array($values)) {
                 // Check the datatype of each value in the array and generate
                 // corresponding typestring $ptype
-                foreach($values as $value) {
-                    if(is_int($value))
+                foreach ($values as $value) {
+                    if (is_int($value))
                         $ptype .= 'i';
-                    elseif(is_float($value))
+                    elseif (is_float($value))
                         $ptype .='d';
                     else
                         $ptype .= 's';
@@ -241,47 +270,47 @@ class Driver_Mysqli
                 }
             }
             else {
-                if(is_int($values))
+                if (is_int($values))
                     $ptype .= 'i';
-                elseif(is_float($values))
+                elseif (is_float($values))
                     $ptype .='d';
                 else
                     $ptype .= 's';
                 $params[] = $values;
             }
-            
+
             //Combine the $params array, $this->stmt and $ptype to be passed as
             //an argument for call_user_func_array()
-            array_unshift($params,$stmt ,$ptype);
+            array_unshift($params, $stmt, $ptype);
             call_user_func_array('mysqli_stmt_bind_param', $this->refValues($params));
 
             //Execute the prepared statment
             $output = $stmt->execute();
 
             // Build the output array if its not a data manipulation query
-            if($isManip === false) {
-                $data   = $stmt->result_metadata();
+            if ($isManip === false) {
+                $data = $stmt->result_metadata();
                 $fields = array();
                 $out = array();
 
                 $fields[0] = $stmt;
                 $count = 1;
 
-                while($field = mysqli_fetch_field($data)) {
+                while ($field = mysqli_fetch_field($data)) {
                     $fields[$count] = &$out[$field->name];
                     $count++;
                 }
                 call_user_func_array('mysqli_stmt_bind_result', $fields);
-                $i=0;
-                while($stmt->fetch()) {
+                $i = 0;
+                while ($stmt->fetch()) {
                     $result[] = $out;
                 }
-               if(count($result) == 1 )
-                  $output =  array_pop($result);
-               else
-                   $output = $result;
+                if (count($result) == 1)
+                    $output = array_pop($result);
+                else
+                    $output = $result;
             }
-            if(isset($output))
+            if (isset($output))
                 return $output;
             else
                 return false;
@@ -290,28 +319,25 @@ class Driver_Mysqli
         // Php version above 5.3 need arguments of mysqli_stmt_bind_param
         // function to be passed by reference. This function will recieve an array
         // and will change each array elements to a reference to array values.
-        function refValues($params)
-        {
-            if (strnatcmp(phpversion(),'5.3') >= 0)
-            {
-                for($i=1; $i<count($params); $i++)
+        function refValues($params) {
+            if (strnatcmp(phpversion(), '5.3') >= 0) {
+                for ($i = 1; $i < count($params); $i++)
                     $params[$i] = &$params[$i];
             }
             return $params;
         }
-        function numColumns()
-        {
+
+        function numColumns() {
             $cols = @mysqli_num_fields($this->result);
         }
 
-        function getColumns()
-        {
+        function getColumns() {
             $column = 0;
             $columns = array();
             $numCols = $this->numColumns();
-            for($column = 0; $column < $numCols; $column++) {
-                   $columnInfo = @mysqli_fetch_field_direct($this->result, $column);
-                   $columns[$columnInfo->name] = $column;
+            for ($column = 0; $column < $numCols; $column++) {
+                $columnInfo = @mysqli_fetch_field_direct($this->result, $column);
+                $columns[$columnInfo->name] = $column;
             }
             print_r($columns);
         }
@@ -322,25 +348,24 @@ class Driver_Mysqli
          * @param mixed $name
          * @param mysqli_stmt $stmt
          */
-        function cacheQuery($name,$stmt)
-        {
+        function cacheQuery($name, $stmt) {
             $this->cachedQueries[$name] = $stmt;
         }
+
         /**
          * Check if query is cached or not. If yes returns the statement object
          * @param mixed $name
          * @return mysqli_stmt
          */
-        function isCached($name)
-        {
-            if(array_key_exists($name, $this->cachedQueries)) {
+        function isCached($name) {
+            if (array_key_exists($name, $this->cachedQueries)) {
                 return $this->cachedQueries[$name];
-            }
-            else {
+            } else {
                 return false;
             }
         }
 
-	
-} # Close class definition
+    }
+
+    # Close class definition
 ?>
